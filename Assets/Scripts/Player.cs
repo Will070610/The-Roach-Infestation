9e19deed88bd571d028488c6144dca4e108a096e
Will070610 <56299676+Will070610@users.noreply.github.com>
2020-01-26 09:26:21 +0100

    Player Jump
    Added player humo in code and also gave a layer name to foreground as ground so that the okayer wont be able to double jump by stating that in the code and doing masking layer on the ground.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,24 +6,29 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Player : MonoBehaviour
 {
     [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
 
     bool isAlive = true;
 
     Rigidbody2D myRigidBody;
     Animator myAnimator;
+    Collider2D myCollider2D;
 
     // Use this for intialization
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
+        myCollider2D = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Run();
+        Jump();
         FlipSprite();
+        
     }
 
     private void Run()
@@ -31,10 +36,21 @@ public class Player : MonoBehaviour
         float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); // value is between -1 to +1
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelocity;
+        
 
         bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
         myAnimator.SetBool("Running", playerHasHorizontalSpeed);
 
+    }
+    private void Jump()
+    {
+        if(!myCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; }
+        
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
+            myRigidBody.velocity += jumpVelocityToAdd;
+        }
     }
 
     private void FlipSprite()
