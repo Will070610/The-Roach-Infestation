6cec8758c71d79ca0dc8913c6fba7abf907e0438
Will070610 <56299676+Will070610@users.noreply.github.com>
2020-01-27 05:48:55 +0100

    Level Exit
    added the enemy and its behaviour. Character death code and animation. Level Exit Door animation.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,12 +8,14 @@ public class Player : MonoBehaviour
     [SerializeField] float runSpeed = 5f;
     [SerializeField] float jumpSpeed = 5f;
     [SerializeField] float climbSpeed = 5f;
+    [SerializeField] Vector2 deathKick = new Vector2(25f, 25f);
 
     bool isAlive = true;
 
     Rigidbody2D myRigidBody;
     Animator myAnimator;
-    Collider2D myCollider2D;
+    CapsuleCollider2D myBodyCollider;
+    BoxCollider2D myFeet;
     float gravityScaleAtStart;
 
     // Use this for intialization
@@ -21,17 +23,21 @@ public class Player : MonoBehaviour
     {
         myRigidBody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
-        myCollider2D = GetComponent<Collider2D>();
+        myBodyCollider = GetComponent<CapsuleCollider2D>();
+        myFeet = GetComponent<BoxCollider2D>();
         gravityScaleAtStart = myRigidBody.gravityScale;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!isAlive) { return; }
+
         Run();
         ClimbLadder();
         Jump();
-        FlipSprite();     
+        FlipSprite();
+        Die();
     }
 
     private void Run()
@@ -48,7 +54,7 @@ public class Player : MonoBehaviour
 
     private void ClimbLadder()
     {
-        if(!myCollider2D.IsTouchingLayers(LayerMask.GetMask("Climbing")))
+        if(!myFeet.IsTouchingLayers(LayerMask.GetMask("Climbing")))
         {
             myAnimator.SetBool("Climbing", false);
             myRigidBody.gravityScale = gravityScaleAtStart;
@@ -67,12 +73,22 @@ public class Player : MonoBehaviour
 
     private void Jump()
     {
-        if(!myCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; }
+        if(!myFeet.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; }
         
         if (CrossPlatformInputManager.GetButtonDown("Jump"))
         {
             Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
             myRigidBody.velocity += jumpVelocityToAdd;
+        }
+    }
+
+    private void Die()
+    {
+        if(myBodyCollider.IsTouchingLayers(LayerMask.GetMask("Enemy")))
+        {
+            isAlive = false;
+            myAnimator.SetTrigger("Dying");
+            GetComponent<Rigidbody2D>().velocity = deathKick;
         }
     }
 
